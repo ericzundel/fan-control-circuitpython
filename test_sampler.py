8412d539a89fbb25f6028277b07a84a0de55b307
Eric Z. Ayers <ericzundel@gmail.com>
2024-01-20 14:42:21 -0500

    Added a unit test for the sampler class

---
+++ test_sampler.py
@@ -0,0 +1,30 @@
+"""test_sampler - some unit tests for the sampler module"""
+
+import unittest
+from sampler import Sampler
+
+
+class TestSampler(unittest.TestCase):
+
+    def test_reset(self):
+        """Test initialization and reset() method"""
+        samples = Sampler(10)
+
+        # There should be no recorded samples
+        last_sample = samples.last()
+        self.assertEqual(0, len(last_sample))
+
+        # Add a sample
+        samples.sample({})
+        last_sample = samples.last()
+        self.assertEqual(1, len(last_sample))
+        self.assertTrue('elapsed_ms' in last_sample)
+
+        samples.reset()
+        # After reset, there should be no recorded samples again
+        last_sample = samples.last()
+        self.assertEqual(0, len(last_sample))
+
+
+if __name__ == "__main__":
+    unittest.main()
